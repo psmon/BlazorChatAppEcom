77b57bdd7f56f0a2ad91913c2d75f94f290c183c
psmon <psmon@live.co.kr>
2021-11-15 00:27:13 +0900

    canvas 리팩토링~

--- BlazorChatApp/Client/ChatLand/ChatLand.cs
+++ BlazorChatApp/Client/ChatLand/ChatLand.cs
@@ -7,102 +7,44 @@ using Blazor.Extensions;
 using Blazor.Extensions.Canvas.Canvas2D;
 
 using BlazorChatApp.Client.ChatLand;
+using BlazorChatApp.Client.Core;
 using BlazorChatApp.Shared;
 
 using Microsoft.AspNetCore.Components;
 
 namespace BlazorChatApp.Client.ChatLand
 {
-    public class ChatLand
+    public class ChatLand : GameContext
     {
         ulong frameCnt = 0;
         private Dictionary<string,ElementReference> resource {get;set; }
         private Field BallField { get;set; }           
         private Canvas2DContext _context { get;set; }
         public DateTime LastRender { get;set; }
+
         public ChatLand()
         {
-            LastRender = DateTime.Now;
+            LastRender = DateTime.Now;            
         }
 
         public static async ValueTask<ChatLand> Create(BECanvasComponent canvas, Dictionary<string,ElementReference> resource )
         {  
+            var canvasContext = await canvas.CreateCanvas2DAsync();
             var game = new ChatLand 
             {   
-                _context = await canvas.CreateCanvas2DAsync(),
-                BallField= new Field(),
+                _context = canvasContext,
+                BallField= new Field(canvasContext),
                 resource = resource
             };
+
+            game.BallField.resource = resource;
+
             return game;
         }
 
-        public async ValueTask GameLoop(float timeStamp, int width, int height)
+        public async ValueTask GameLoop(float timeStamp)
         {
-            frameCnt++;
 
-            if(frameCnt > int.MaxValue )
-            {
-                frameCnt = 0;
-            }
-
-            if(frameCnt%(60*5)==0)
-            {
-                BallField.SyncPos();
-            }
-            else
-            {
-                BallField.StepForward();
-            }
-
-            double fps = 1.0 / (DateTime.Now - LastRender).TotalSeconds;
-            LastRender = DateTime.Now;
-
-            await _context.BeginBatchAsync();
-            await _context.ClearRectAsync(0, 0, BallField.Width, BallField.Height);
-            await _context.SetFillStyleAsync("#003366");
-            await _context.FillRectAsync(0, 0, BallField.Width, BallField.Height);
-            await _context.DrawImageAsync(resource["img-back"], 0, 0,BallField.Width,BallField.Height);
-
-            await _context.SetFontAsync("26px Segoe UI");
-            await _context.SetFillStyleAsync("#FFFFFF");
-
-
-            await _context.FillTextAsync("Blazor WebAssembly + HTML Canvas", 10, 30);
-            await _context.SetFontAsync("16px consolas");
-            await _context.FillTextAsync($"FPS: {fps:0.000}", 10, 50);
-            await _context.SetStrokeStyleAsync("#FFFFFF");
-
-            await _context.SetFontAsync("12px 바탕체");
-            await _context.SetFillStyleAsync("Red");
-            await _context.SetStrokeStyleAsync("#DF0101");
-
-            foreach(var store in BallField.storeLinks)
-            {
-                await _context.FillTextAsync($"{store.Name}-{store.PosX},{store.PosY}", store.PosX, store.PosY);
-            }
-
-            await _context.SetFillStyleAsync("White");
-            await _context.SetStrokeStyleAsync("#FFFFFF");
-            foreach (var ball in BallField.Balls)
-            {
-                if (!string.IsNullOrEmpty(ball.ChatMessage))
-                {
-                    await _context.FillTextAsync($"{ball.Name} - {ball.ChatMessage}", ball.X -10, ball.Y -10);
-                }
-                else
-                {
-                    await _context.FillTextAsync($"{ball.Name} - {(int)ball.X},{(int)ball.Y}", ball.X -10, ball.Y -10);
-                }
-
-
-                await _context.DrawImageAsync(resource["img-char1"], ball.X,ball.Y,80,80);
-                //await _context.BeginPathAsync();
-                //await _context.ArcAsync(ball.X, ball.Y, ball.Radius, 0, 2 * Math.PI, false);
-                //await _context.SetFillStyleAsync(ball.Color);
-                //await _context.FillAsync();
-                //await _context.StrokeAsync();
-            }
-            await _context.EndBatchAsync();
         }
 
 
@@ -132,5 +74,14 @@ namespace BlazorChatApp.Client.ChatLand
             return BallField.CollisionCheck(x, y);
         }
 
+        protected override async ValueTask Update()
+        {
+            await BallField.Update();
+        }
+
+        protected override async ValueTask Render()
+        {
+            await BallField.Render();
+        }
     }
 }
--- BlazorChatApp/Client/ChatLand/Field.cs
+++ BlazorChatApp/Client/ChatLand/Field.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
+using Blazor.Extensions.Canvas.Canvas2D;
+
+using BlazorChatApp.Client.Core;
 using BlazorChatApp.Shared;
+
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorChatApp.Client.ChatLand
 {
@@ -16,13 +22,21 @@ namespace BlazorChatApp.Client.ChatLand
 
     public class Field
     {
+        private Canvas2DContext _context;
+
+        public DateTime LastRender { get;set; }
+
+        public Dictionary<string,ElementReference> resource {get;set; }
+
         public readonly List<StoreLink> storeLinks= new List<StoreLink>();
         public readonly List<Ball> Balls = new List<Ball>();
         public double Width { get; private set; } = 800;
         public double Height { get; private set; } = 600;
 
-        public Field()
+        public Field(Canvas2DContext canvas2DContext)
         {
+            _context = canvas2DContext;
+            
             storeLinks.Add(new StoreLink()
             { 
                 Name = "A몰",
@@ -86,10 +100,12 @@ namespace BlazorChatApp.Client.ChatLand
         public void Resize(double width, double height) =>
             (Width, Height) = (width, height);
 
-        public void StepForward()
+        public Task StepForward()
         {            
             foreach (Ball ball in Balls)
                 ball.StepForward();
+
+            return Task.CompletedTask;
         }
 
         public void SyncPos()
@@ -156,5 +172,55 @@ namespace BlazorChatApp.Client.ChatLand
             }
         }
 
+        public async ValueTask Update()
+        {
+            await StepForward();
+        }
+
+        public async ValueTask Render()
+        {            
+            double fps = 1.0 / (DateTime.Now - LastRender).TotalSeconds;
+            LastRender = DateTime.Now;
+
+            await _context.BeginBatchAsync();
+            await _context.ClearRectAsync(0, 0, Width, Height);
+            await _context.SetFillStyleAsync("#003366");
+            await _context.FillRectAsync(0, 0, Width, Height);
+            await _context.DrawImageAsync(resource["img-back"], 0, 0, Width, Height);
+
+            await _context.SetFontAsync("26px Segoe UI");
+            await _context.SetFillStyleAsync("#FFFFFF");
+
+
+            await _context.FillTextAsync("Blazor WebAssembly + HTML Canvas", 10, 30);
+            await _context.SetFontAsync("16px consolas");
+            await _context.FillTextAsync($"FPS: {fps:0.000}", 10, 50);
+            await _context.SetStrokeStyleAsync("#FFFFFF");
+
+            await _context.SetFontAsync("12px 바탕체");
+            await _context.SetFillStyleAsync("Red");
+            await _context.SetStrokeStyleAsync("#DF0101");
+
+            foreach(var store in storeLinks)
+            {
+                await _context.FillTextAsync($"{store.Name}-{store.PosX},{store.PosY}", store.PosX, store.PosY);
+            }
+
+            await _context.SetFillStyleAsync("White");
+            await _context.SetStrokeStyleAsync("#FFFFFF");
+            foreach (var ball in Balls)
+            {
+                if (!string.IsNullOrEmpty(ball.ChatMessage))
+                {
+                    await _context.FillTextAsync($"{ball.Name} - {ball.ChatMessage}", ball.X -10, ball.Y -10);
+                }
+                else
+                {
+                    await _context.FillTextAsync($"{ball.Name} - {(int)ball.X},{(int)ball.Y}", ball.X -10, ball.Y -10);
+                }
+                await _context.DrawImageAsync(resource["img-char1"], ball.X,ball.Y,80,80);                
+            }
+            await _context.EndBatchAsync();
+        }
     }
 }
